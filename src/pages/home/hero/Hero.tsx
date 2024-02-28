18c7a2911f31ce8db453c4b522c8b6575db4be36
IjjazAhmad <ijjazahmad705@gmail.com>
2024-02-28 17:44:10 +0500

    fix swiper arrow

--- src/pages/home/hero/Hero.tsx
+++ src/pages/home/hero/Hero.tsx
@@ -3,6 +3,8 @@ import HeroCard from '../../../components/heroCard/HeroCard'
 import logo1 from '../../../assets/images/Product/logo1.png'
 import logo2 from '../../../assets/images/Product/logo2.png'
 import logo3 from '../../../assets/images/Product/logo3.png'
+import swiperArrowLeft from '../../../assets/icons/swiperArrowLeft.svg'
+import swiperArrowRight from '../../../assets/icons/swiperArrowRight.svg'
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -13,52 +15,64 @@ import HeroSlideCard from '../../../components/heroSlideCard/HeroSlideCard'
 
 export default function Hero() {
     return (
-        <div className="hero px-[15px] w-[100%] md:px-[50px] lg:px-[60px] xl:px-[70px]">
+        <>
+            <div className="hero px-[15px] w-[100%] md:px-[50px] lg:px-[60px] xl:px-[70px]">
 
-            <Swiper
+                <Swiper
 
-                modules={[Navigation, Pagination, Scrollbar, A11y]}
-                spaceBetween={20}
-                pagination={{ clickable: true }}
-            >
-                <SwiperSlide>
-                    <HeroSlideCard />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <HeroSlideCard />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <HeroSlideCard />
-                </SwiperSlide>
+                    modules={[Navigation, Pagination, Scrollbar, A11y]}
+                    spaceBetween={20}
+                    pagination={{ clickable: true }}
+                >
+                    <SwiperSlide>
+                        <HeroSlideCard />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <HeroSlideCard />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <HeroSlideCard />
+                    </SwiperSlide>
 
-            </Swiper>
-            <Swiper
+                </Swiper>
+                <div className='relative'>
+                    <Swiper
 
-                modules={[Navigation, Pagination, Scrollbar, A11y]}
-                navigation
-                breakpoints={{
+                        modules={[Navigation, Pagination, Scrollbar, A11y]}
+                        navigation={{
+                            prevEl: '.swiper-button-prev',
+                            nextEl: '.swiper-button-next',
+                        }}
+                        breakpoints={{
 
-                    576: {
-                        slidesPerView: 1,
-                    },
+                            576: {
+                                slidesPerView: 1,
+                            },
 
-                    768: {
-                        slidesPerView: 2,
-                    },
+                            768: {
+                                slidesPerView: 2,
+                            },
 
-                    992: {
-                        slidesPerView: 3,
-                    },
-                }}
-            >
-                <SwiperSlide> <HeroCard img={logo1} title='Speaker' /></SwiperSlide>
-                <SwiperSlide> <HeroCard img={logo2} title='Desktop & laptop' /></SwiperSlide>
-                <SwiperSlide> <HeroCard img={logo3} title='DSLR camera' /></SwiperSlide>
-                <SwiperSlide> <HeroCard img={logo1} title='Speaker' /></SwiperSlide>
-                <SwiperSlide> <HeroCard img={logo2} title='Desktop & laptop' /></SwiperSlide>
-                <SwiperSlide> <HeroCard img={logo3} title='DSLR camera' /></SwiperSlide>
-
-            </Swiper>
-        </div>
+                            992: {
+                                slidesPerView: 3,
+                            },
+                        }}
+                    >
+                        <SwiperSlide> <HeroCard img={logo1} title='Speaker' /></SwiperSlide>
+                        <SwiperSlide> <HeroCard img={logo2} title='Desktop & laptop' /></SwiperSlide>
+                        <SwiperSlide> <HeroCard img={logo3} title='DSLR camera' /></SwiperSlide>
+                        <SwiperSlide> <HeroCard img={logo1} title='Speaker' /></SwiperSlide>
+                        <SwiperSlide> <HeroCard img={logo2} title='Desktop & laptop' /></SwiperSlide>
+                        <SwiperSlide> <HeroCard img={logo3} title='DSLR camera' /></SwiperSlide>
+                    </Swiper>
+                    <div className="swiper-button-prev  ">
+                        <img src={swiperArrowLeft} alt='..' />
+                    </div>
+                    <div className="swiper-button-next">
+                        <img src={swiperArrowRight} alt='..' />
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
